2c3d12601554ad3d9119845d6e4f408472a3eeb0
ptt <ptt99931.work@gmail.com>
2024-11-26 22:54:45 +0700

    [tracking] refactor

--- lib/core/models/map_current_location_style.dart
+++ lib/core/models/map_current_location_style.dart
@@ -1,5 +1,19 @@
 enum MapCurrentLocationStyle {
   normal,
   navigation,
-  tracking,
+  tracking;
+
+  static List<MapCurrentLocationStyle> hereStylesGroup = [
+    normal,
+    navigation,
+  ];
+  static List<MapCurrentLocationStyle> _customStypesGroup = [tracking];
+  static List<List<MapCurrentLocationStyle>> _styleGroups = [
+    hereStylesGroup,
+    _customStypesGroup,
+  ];
+  bool isDifferentStyleGroup(MapCurrentLocationStyle style) {
+    return _styleGroups.indexWhere((styles) => styles.contains(this)) !=
+        _styleGroups.indexWhere((styles) => styles.contains(style));
+  }
 }
